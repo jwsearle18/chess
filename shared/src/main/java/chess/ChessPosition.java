7084e31271babd2db88654cb83f6366786341eaf
jwsearle18 <144712016+jwsearle18@users.noreply.github.com>
2024-01-19 13:11:32 -0700

    fix(tests) create override methods

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -33,4 +33,19 @@ public class ChessPosition {
 
         return col;
     }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
