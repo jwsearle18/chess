d06cc45b841744d5e64bbe6ece6558541b91dcf6
jwsearle18 <144712016+jwsearle18@users.noreply.github.com>
2024-02-28 20:09:26 -0700

    login test

--- server/src/test/java/serviceTests/LoginTests.java
+++ server/src/test/java/serviceTests/LoginTests.java
@@ -1,4 +1,58 @@
 package serviceTests;
 
+import Failures.F401;
+import dataAccess.DataAccessException;
+import dataAccess.MemoryUserDAO;
+import dataAccess.UserDAO;
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import requests.LoginRequest;
+import service.LoginService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LoginTests {
+    static LoginService loginService;
+    static UserDAO userDAO;
+
+    @BeforeAll
+    static void setup() {
+        loginService = new LoginService();
+        userDAO = new MemoryUserDAO();
+    }
+
+    @BeforeEach
+    void resetDAO() throws DataAccessException {
+        userDAO.clear();
+    }
+
+    @Test
+    void loginTestPositive() throws Exception {
+        UserData user = new UserData("you", "pass", "email@email.email");
+        userDAO.createUser(user);
+        LoginRequest loginRequest = new LoginRequest("you", "pass");
+
+        AuthData authData = loginService.login((loginRequest));
+
+        assertNotNull(authData);
+
+    }
+
+    @Test
+    void loginTestNegative() throws Exception{
+        UserData user = new UserData("you", "pass", "email@email.email");
+        userDAO.createUser(user);
+        LoginRequest badLoginRequest = new LoginRequest("you", "word");
+
+        Exception exception = assertThrows(F401.class, () -> {
+            loginService.login(badLoginRequest);
+        });
+
+        String expectedMessage = "Error: unauthorized";
+        assertEquals(exception.getMessage(), expectedMessage);
+    }
 }
+
