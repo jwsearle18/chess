68c6f50035e0a0133b742c4272ff84b86d50bedb
jwsearle18 <144712016+jwsearle18@users.noreply.github.com>
2024-03-07 15:04:22 -0700

    SQLUserDAO methods creation

--- server/src/main/java/dataAccess/SQLUserDAO.java
+++ server/src/main/java/dataAccess/SQLUserDAO.java
@@ -1,4 +1,62 @@
 package dataAccess;
 
-public class SQLUserDAO {
+import model.UserData;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static dataAccess.DatabaseManager.getConnection;
+
+public class SQLUserDAO implements UserDAO {
+
+    @Override
+    public void clear() throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection();
+             Statement statement = conn.createStatement()) {
+            statement.executeUpdate("TRUNCATE users");
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void createUser(UserData userData) throws DataAccessException {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String hashedPassword = encoder.encode(userData.password());
+
+        try(Connection conn = DatabaseManager.getConnection();
+            PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO users (username, password, email) VALUES (?, ?, ?)")) {
+            preparedStatement.setString(1, userData.username());
+            preparedStatement.setString(2, hashedPassword);
+            preparedStatement.setString(3, userData.email());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection();
+        PreparedStatement preparedStatement = conn.prepareStatement("SELECT username, password, email FROM users WHERE username=?")) {
+            preparedStatement.setString(1, username);
+            try(var rs = preparedStatement.executeQuery()) {
+                while(rs.next()) {
+                    var storedUsername = rs.getString("username");
+                    var password = rs.getString("password");
+                    var email = rs.getString("email");
+                    return new UserData(storedUsername, password, email);
+                }
+            } catch (SQLException e) {
+                throw new DataAccessException(e.getMessage());
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+        return null;
+    }
 }
