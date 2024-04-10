50654c6915cf5e5f06a3dde3e8d3e11bf62e1f96
jwsearle18 <144712016+jwsearle18@users.noreply.github.com>
2024-04-10 10:39:04 -0600

    code quality updates

--- client/src/main/java/ClientMain.java
+++ client/src/main/java/ClientMain.java
@@ -4,18 +4,6 @@ public class ClientMain {
 
 
     public static void main(String[] args) {
-//        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
-//
-//        out.print(ERASE_SCREEN);
-//
-//        ChessBoard board = new ChessBoard();
-//        board.resetBoard();
-//        printChessBoards(out, board, true);
-//
-//        out.print(SET_BG_COLOR_BLACK);
-//        out.print(SET_TEXT_COLOR_WHITE);
-//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-//        System.out.println("♕ 240 Chess Client: " + piece);
         int desiredPort = 4040;
         if (args.length > 0) {
             desiredPort = Integer.parseInt(args[0]);
